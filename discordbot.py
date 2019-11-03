35e359bd0c8944544ff896213a4843c32174a405
rito-maker <57310609+rito-maker@users.noreply.github.com>
2019-11-03 15:47:41 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -15,5 +15,16 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
+@bot.event
+async def on_message(message):
+    # 「おはよう」で始まるか調べる
+    if message.content.startswith("おはよう"):
+        # 送り主がBotだった場合反応したくないので
+        if client.user != message.author:
+            # メッセージを書きます
+            m = "おはようございます" + message.author.name + "さん！"
+            # メッセージが送られてきたチャンネルへメッセージを送ります
+            await message.channel.send(m)
+
 
 bot.run(token)
