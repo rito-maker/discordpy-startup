d70dcd3a6fa8f7541a179c7d1b0040b66eb61699
rito-maker <57310609+rito-maker@users.noreply.github.com>
2019-11-03 16:08:00 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,50 +1,22 @@
-#from discord.ext import commands
-#import os
-#import traceback
+from discord.ext import commands
+import os
+import traceback
 
-#bot = commands.Bot(command_prefix='/')
-#token = os.environ['DISCORD_BOT_TOKEN']
-
-
-#@bot.event
-#async def on_command_error(ctx, error):
-#    await ctx.send(str(error))
+bot = commands.Bot(command_prefix='/')
+token = os.environ['DISCORD_BOT_TOKEN']
 
 
-#@bot.command()
-#async def ping(ctx):
-#    await ctx.send('pong')
+@bot.event
+async def on_command_error(ctx, error):
+    await ctx.send(str(error))
 
-#@bot.command()
-#async def neko(ctx):
-#    await ctx.send('にゃーん！')
 
-#bot.run(token)
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
 
-# インストールした discord.py を読み込む
-import discord
+@bot.command()
+async def neko(ctx):
+    await ctx.send('にゃーん！')
 
-# 自分のBotのアクセストークンに置き換えてください
-TOKEN = 'THi5IsDuMMyaCCesSTOK3nQ4.Cl2FMQ.ThIsi5DUMMyAcc3s5ToKen7kKWs'
-
-# 接続に必要なオブジェクトを生成
-client = discord.Client()
-
-# 起動時に動作する処理
-@client.event
-async def on_ready():
-    # 起動したらターミナルにログイン通知が表示される
-    print('ログインしました')
-
-# メッセージ受信時に動作する処理
-@client.event
-async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
-    if message.author.bot:
-        return
-    # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == '/neko':
-        await message.channel.send('にゃーん')
-
-# Botの起動とDiscordサーバーへの接続
-client.run(TOKEN)
+bot.run(token)
